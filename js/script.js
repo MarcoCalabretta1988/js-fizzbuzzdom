64e355c3e74ed3b02142c0f83ecc5ae7c250c9b3
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2022-12-07 13:36:58 +0100

    End Base logic

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-// JS LINK
+// ? JS LINK
 
 console.log('JS OK');
 
@@ -33,3 +33,40 @@ BONUS 2:
 Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
 Come abbiamo visto puoi  usare varie tecniche (style , className, classList)
 */
+
+
+
+// ? BASE TO DO LIST 
+/*
+1- Creare un ciclo che va da 1 a 100;
+2- Creare una variabile da stampare in console;
+3- Effettuare controlli se divisibile per 3,5,3 e 5;
+4- Variare il valore della variabile a seconda dei controlli;
+5- Stampare in console;
+*/
+
+
+//1- Creare un ciclo che va da 1 a 100;
+
+for(let i = 1 ; i <= 100 ; i++){
+
+    //2- Creare una variabile da stampare in console;
+    let message = i;
+    
+    //3- Effettuare controlli se divisibile per 3,5,3 e 5;
+    if ( (i % 3) === 0){
+        //4- Variare il valore della variabile a seconda dei controlli;
+        message = 'Fizz';
+    }
+    else if( (i % 5) === 0){
+        //4- Variare il valore della variabile a seconda dei controlli;
+        message = 'Buzz';
+    }
+
+    if ((i % 3) === 0 && (i % 5) === 0){
+        message = 'FizzBuzz';
+    }
+    
+    //5- Stampare in console;
+    console.log(message);
+}
